19d073294ae682e6215fc4681d944f3fb9b2532a
shiju k <shijukk1997@gmail.com>
2024-04-23 12:10:18 +0530

    input text display updated and delete function updated

--- FrontEnd/note/src/Components/Body/Note.jsx
+++ FrontEnd/note/src/Components/Body/Note.jsx
@@ -1,33 +1,64 @@
- import React,{useState} from 'react'
- import './note.css'
- import axios from 'axios'
- 
- const Note = () => {
-  const [content,setContent]=useState("")
-  console.log(content)
-  const handlechange =(e) =>{
-    e.preventDefault()
-    setContent(e.target.value)
+import React, { useState } from "react";
+import "./note.css";
+import axios from "axios";
 
-  }
+const Note = () => {
+  const [content, setContent] = useState("");
+  const [displaycontent, setDisplaycontent] = useState([]);
 
-  const handleaddText = async () =>{
+
+  const handlechange = (e) => {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+
+
+  const handleaddText = async () => {
     try {
-      const response= await axios.post('http://localhost:5000/api/add',{content})
+      if (content.trim() !== "") {
+        setDisplaycontent([...displaycontent, content]);
+        setContent("");
+      }
+
+      const response = await axios.post("http://localhost:5000/api/add", {
+        content,
+      });
       console.log(response.data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
+  };
+
+
+  const handleDelete = (index) =>{
+    const newdisplaycontent = [...displaycontent]
+    newdisplaycontent.splice(index,1)
+    setDisplaycontent(newdisplaycontent)
   }
-   return (
-     <div  className='container' >
-      <div className='TextField'>
-        <input  type='text' placeholder='Take a note ...' onChange={handlechange} value={content} />
+  return (
+    <div className="container">
+      <div className="TextField">
+        <div className="fixedfield">
+        <input
+          type="text"
+          placeholder="Take a note ..."
+          onChange={handlechange}
+          value={content}
+        />
         <button onClick={handleaddText}>ADD</button>
-        {content}
-      </div>
-     </div>
-   )
- }
- 
- export default Note
+        </div>
+      <div className="display">
+        {displaycontent.map((value, index) => {
+          return (
+            <div className="note" key={index}><p>{value}</p>
+            <button onClick={()=>{handleDelete(index)}}>Delete</button>
+            </div>
+          )
+        })}
+        </div>
+        </div>
+        </div>
+  );
+};
+
+export default Note;
