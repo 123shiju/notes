87c6d09337bdb5be0fbee17cec94201085577c7e
shiju k <shijukk1997@gmail.com>
2024-04-29 12:17:22 +0530

    concurrently running error solved , updated everything

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,26 +1,30 @@
-const express=require('express')
-const app=express()
-const morgan=require('morgan')
-const cors=require('cors')
-const { urlencoded } = require('body-parser')
-const bodyparser=require('body-parser')
-const Routes=require('./Routes/users.js')
-const pool=require('./db')
-
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const { urlencoded } = require("body-parser");
+const bodyparser = require("body-parser");
+const Routes = require("./Routes/users.js");
+const pool = require("./db");
 
 //middlewares
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
-app.use(bodyparser.urlencoded({ extended:true }))
-app.use('/api',Routes);
-const PORT=5000
+app.use(morgan("dev"));
 
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use("/api", Routes);
 
-app.listen(PORT,(err)=>{
-    if(!err){
-        console.log("server is running on port",`${PORT}`)
-    }else{
-        console.log("error occured",err)
-    }
-})
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log("server is running on port", `${PORT}`);
+  } else {
+    console.log("error occured", err);
+  }
+});
